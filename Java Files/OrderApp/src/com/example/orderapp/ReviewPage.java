6d89805c9eafb10ee8f481edcfa2af6267e46767
g8-gk <choganji@naver.com>
2014-03-18 18:29:00 -0700

    wtf

--- Java Files/OrderApp/src/com/example/orderapp/ReviewPage.java
+++ Java Files/OrderApp/src/com/example/orderapp/ReviewPage.java
@@ -15,7 +15,25 @@ public class ReviewPage extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_review_page);
 		
+		Bundle extras = getIntent().getExtras();
+		if (extras != null)
+		    Alcohol = extras.getIntArray("Alcohol_Data");
 		
+		
+		StringBuilder strb = new StringBuilder("Order: \n");
+		
+		for(int i=0;i<Alcohol.length;i++){
+			if(Alcohol[i]!=0){
+				strb.append(com.example.orderapp.Alcohol.getAlcohols(i)+":   "+ Alcohol[i]+ "\n");				
+			}
+		}
+		
+	
+		TextView textView1 = new TextView(this);
+		textView1.setText(strb);
+		
+	
+
 		
 	}
 
@@ -23,43 +41,9 @@ public class ReviewPage extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.review_page, menu);
-		
-		
-		
-		Bundle extras = getIntent().getExtras();
-		if (extras != null)
-		    Alcohol = extras.getIntArray("Alcohol_Data");
-		
-		StringBuilder strb = new StringBuilder("Order: \n");
-		int i;
-		for(i=0;i<Alcohol.length;i++){
-			if(Alcohol[i]!=0){
-				strb.append(com.example.orderapp.Alcohol.getAlcohols(i)+":   "+ Alcohol[i]+ "\n");				
-			}
-		}
-		
-		
-		LinearLayout top_linear_layout =(LinearLayout) findViewById(R.id.LinearLayout1);
-		TextView textView1 = new TextView(this);
-		textView1.setText(strb);
-		top_linear_layout.addView(textView1);
-		
-		
-		
 		return true;
 	}
 	
-
-	
-//	strb.append( "Total items ordered = " + total_items + "\n");
-//	strb.append( "Total Cost = " + total_price + "\n");
-//	// Rather than writing to a pre-allocated field on the
-//	// screen, create a new view, and write to it.
-//	LinearLayout top_linear_layout =
-//	(LinearLayout) findViewById(R.id.LinearLayout1);
-//	TextView report_line = new TextView(this);
-//	report_line.setText(strb);
-//	top_linear_layout.addView(report_line);
 
 
 }
