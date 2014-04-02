1b71a604dc200f3446697790d475d4c4fa1bfacf
FelixTheCat29 <acegi137@gmail.com>
2014-04-01 20:10:16 -0700

    settings 2

--- Java Files/ChefApp/src/com/example/chefapp/MainPage.java
+++ Java Files/ChefApp/src/com/example/chefapp/MainPage.java
@@ -1,9 +1,10 @@
 package com.example.chefapp;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainPage extends Activity {
@@ -20,10 +21,25 @@ public class MainPage extends Activity {
 		getMenuInflater().inflate(R.menu.main_page, menu);
 		return true;
 	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_settings:
+	    	    Intent intent = new Intent(this, Connection.class);
+	    	    startActivity(intent); 
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
 
 	public void Transistion(View view){
 	    Intent intent = new Intent(this, Connection.class);
 	    startActivity(intent); 
 		
 	}
+	
+	
 }
