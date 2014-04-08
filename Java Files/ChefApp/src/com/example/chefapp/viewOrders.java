85a7597774b552a580440e86f9985aaa31b66c71
FelixTheCat29 <acegi137@gmail.com>
2014-04-07 21:09:30 -0700

    for Johnny

--- Java Files/ChefApp/src/com/example/chefapp/viewOrders.java
+++ Java Files/ChefApp/src/com/example/chefapp/viewOrders.java
@@ -5,7 +5,6 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
@@ -15,12 +14,12 @@ import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.ViewGroup.LayoutParams;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+
+
 
 public class viewOrders extends Activity {
 	public static List<String> orders = new ArrayList<String>();
@@ -29,7 +28,7 @@ public class viewOrders extends Activity {
 	public static List<Integer> clientIDList = new ArrayList<Integer>();
 	public static Handler mHandler;
 	int numA = 0;
-	
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +36,21 @@ public class viewOrders extends Activity {
 		// setContentView(R.layout.activity_view_orders);
 		mHandler = new Handler() {
 			public void handleMessage(Message msg) {
-				//Log.i("UI", "" + stringForOrders);
-				viewOrders.orders.add(stringForOrders);
+				Log.i("UI", "" + stringForOrders);
+				//viewOrders.orders.add(stringForOrders);
+				
+				String[][] Customer = new String[][]{};
+				String[] parts = stringForOrders.split("\\*"); 
+				int numberCustomers = parts.length;
+				for(int i=0; i<numberCustomers; i++)
+				{
+					Customer[i] = parts[i].split("\\/");
+
+					//Customer is an array of array of strings
+					//Elements of Customer are String[]
+
+				}
+
 				onResume();
 			}
 		};
@@ -48,101 +60,176 @@ public class viewOrders extends Activity {
 	protected void onResume() {
 		super.onResume();
 		
-		Log.i("MY_MESSAGE", "in onResume (ViewOrdersPage)");
-		final LinearLayout parentlayout = new LinearLayout(this);
 		ScrollView scrollview;
 		scrollview = new ScrollView(this);
-		parentlayout.setOrientation(LinearLayout.VERTICAL);
-		scrollview.addView(parentlayout);
-		TextView a,b;
-		int ind;
+		LinearLayout linearlayout = new LinearLayout(this);
+		linearlayout.setOrientation(LinearLayout.VERTICAL);
+		scrollview.addView(linearlayout);
+		TextView b;
+		TextView a;
+		TextView c;
+
+//		String[][] Customer = new String[][]{};
+				
+//		String[] parts = stringForOrders.split("\\*"); 
+//		int numberCustomers = parts.length;
+//		for(int i=0; i<numberCustomers; i++)
+//		{
+//			Customer[i] = parts[i].split("\\/");
+//
+//			//Customer is an array of array of strings
+//			//Elements of Customer are String[]
+//
+//		}
+
+
 		
+		String[] menuItems = {"Kokanee","Budweiser","Chivas","Ballantines","Pizza","Hamburger","Sandwich","French Fries","Edamame","Salad","Cheese","Special 1","Special 2"};
+		String special1 = menuItems[11];
+		String special2 = menuItems[12];
+		//When chef name special1 and special2, save them here
 
-		for (ind=0; ind < orders.size();ind++) {
-			String s = orders.get(ind).toString();
-			int sentinel = s.indexOf('#');
-			String str = s.substring(0,sentinel);
-			int clientID = Integer.parseInt(str);
-			clientIDList.add(clientID); 
-			s = s.replace(str+"#","");
-			
-			final LinearLayout linearHeader = new LinearLayout(this);
-			final LinearLayout linear0 = new LinearLayout(this);
-			final LinearLayout linear1 = new LinearLayout(this);
-			final LinearLayout linearGap = new LinearLayout(this);
-			linearHeader.setOrientation(LinearLayout.HORIZONTAL);
+		for(int j=0; j<numberCustomers; j++)
+		{
+
+			LinearLayout linear0 = new LinearLayout(this);
 			linear0.setOrientation(LinearLayout.HORIZONTAL);
-			linear1.setOrientation(LinearLayout.HORIZONTAL);
-			linearGap.setOrientation(LinearLayout.HORIZONTAL);
-			parentlayout.addView(linearHeader);
-			parentlayout.addView(linear0);
-			parentlayout.addView(linear1);
-			parentlayout.addView(linearGap);
-			
+			linearlayout.addView(linear0);
 			a = new TextView(this);
-			
-
-			a.setId(ind);
-				a.setText("Client #"+ clientID +":");	
-			a.setTextSize(15);
+			a.setText("Below is the order for guest " + (j+1) + ":");
+			a.setId(50);
+			a.setTextSize(25);
 			a.setPadding(18, 13, 18, 13);
-			a.setTypeface(null, Typeface.BOLD);
-			a.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-					LayoutParams.WRAP_CONTENT));
-			linearHeader.addView(a);
-			
-			
-			b = new TextView(this);
-			b.setText(s);
-			b.setId(50);
-			b.setTextSize(15);
-			b.setPadding(18, 13, 18, 13);
-			b.setTypeface(Typeface.SERIF, Typeface.BOLD_ITALIC);
-			b.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-					LayoutParams.WRAP_CONTENT));
-			linear0.addView(b);
-			
-			Button d = new Button(this);
-			d.setText("Done");
-			d.setId(ind);
-			d.setTextSize(15);
-			d.setPadding(8, 3, 8, 3);
-			d.setTypeface(Typeface.SERIF, Typeface.BOLD_ITALIC);
-			d.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
-					LayoutParams.WRAP_CONTENT));
-			linear1.addView(d);
-			
-			TextView e = new TextView(this);
-			e.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
-					40));
-			linearGap.addView(e);
-			
-			d.setOnClickListener(new View.OnClickListener(){
-
-				@Override
-				public void onClick(View v) {
-					parentlayout.removeView(linearHeader);
-					parentlayout.removeView(linear0);
-					parentlayout.removeView(linear1);
-					parentlayout.removeView(linearGap);
-					clientIDList.remove(v.getId());
-					orders.remove(v.getId());
-					sendAlert(clientIDList.get(v.getId()));
-				}
-			});
-			
+			a.setTypeface(Typeface.SERIF,Typeface.BOLD_ITALIC);
+			a.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
+			linear0.addView(a);
 			this.setContentView(scrollview);
 
+
+
+
+			for(int i = 0 ; i < Customer[j].length ; i++)
+			{	
+				//int array1[] = Customer[j];
+				if (!Customer[j].equals("0"))
+
+				{	
+					LinearLayout linear1 = new LinearLayout(this);
+					linear1.setOrientation(LinearLayout.HORIZONTAL);
+					linearlayout.addView(linear1);
+					b = new TextView(this);
+					b.setText(menuItems[i]+": "+ Customer[j]);
+					b.setId(i);
+					b.setTextSize(20);
+					b.setPadding(18, 13, 18, 13);
+					b.setTypeface(Typeface.SERIF,Typeface.BOLD_ITALIC);
+					b.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
+
+					linear1.addView(b);
+				}
+
+				this.setContentView(scrollview);
+			}
 		}
+
+		//Kokanee, Budweiser, Chivas, Ballantines, Pizza, Hamburger, Sandwich, French Fries, Edamame, Salad, Cheese, Special 1, Special 2
+
+
+		//		Log.i("MY_MESSAGE", "in onResume (ViewOrdersPage)");
+		//		final LinearLayout parentlayout = new LinearLayout(this);
+		//		ScrollView scrollview;
+		//		scrollview = new ScrollView(this);
+		//		parentlayout.setOrientation(LinearLayout.VERTICAL);
+		//		scrollview.addView(parentlayout);
+		//		TextView a,b;
+		//		int ind;
+		//		
+		//
+		//		for (ind=0; ind < orders.size();ind++) {
+		//			String s = orders.get(ind).toString();
+		//			int sentinel = s.indexOf('#');
+		//			String str = s.substring(0,sentinel);
+		//			int clientID = Integer.parseInt(str);
+		//			clientIDList.add(clientID); 
+		//			s = s.replace(str+"#","");
+		//			
+		//			final LinearLayout linearHeader = new LinearLayout(this);
+		//			final LinearLayout linear0 = new LinearLayout(this);
+		//			final LinearLayout linear1 = new LinearLayout(this);
+		//			final LinearLayout linearGap = new LinearLayout(this);
+		//			linearHeader.setOrientation(LinearLayout.HORIZONTAL);
+		//			linear0.setOrientation(LinearLayout.HORIZONTAL);
+		//			linear1.setOrientation(LinearLayout.HORIZONTAL);
+		//			linearGap.setOrientation(LinearLayout.HORIZONTAL);
+		//			parentlayout.addView(linearHeader);
+		//			parentlayout.addView(linear0);
+		//			parentlayout.addView(linear1);
+		//			parentlayout.addView(linearGap);
+		//			
+		//			a = new TextView(this);
+		//			
+		//
+		//			a.setId(ind);
+		//				a.setText("Client #"+ clientID +":");	
+		//			a.setTextSize(15);
+		//			a.setPadding(18, 13, 18, 13);
+		//			a.setTypeface(null, Typeface.BOLD);
+		//			a.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
+		//					LayoutParams.WRAP_CONTENT));
+		//			linearHeader.addView(a);
+		//			
+		//			
+		//			b = new TextView(this);
+		//			b.setText(s);
+		//			b.setId(50);
+		//			b.setTextSize(15);
+		//			b.setPadding(18, 13, 18, 13);
+		//			b.setTypeface(Typeface.SERIF, Typeface.BOLD_ITALIC);
+		//			b.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
+		//					LayoutParams.WRAP_CONTENT));
+		//			linear0.addView(b);
+		//			
+		//			Button d = new Button(this);
+		//			d.setText("Done");
+		//			d.setId(ind);
+		//			d.setTextSize(15);
+		//			d.setPadding(8, 3, 8, 3);
+		//			d.setTypeface(Typeface.SERIF, Typeface.BOLD_ITALIC);
+		//			d.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
+		//					LayoutParams.WRAP_CONTENT));
+		//			linear1.addView(d);
+		//			
+		//			TextView e = new TextView(this);
+		//			e.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
+		//					40));
+		//			linearGap.addView(e);
+		//			
+		//			d.setOnClickListener(new View.OnClickListener(){
+		//
+		//				@Override
+		//				public void onClick(View v) {
+		//					parentlayout.removeView(linearHeader);
+		//					parentlayout.removeView(linear0);
+		//					parentlayout.removeView(linear1);
+		//					parentlayout.removeView(linearGap);
+		//					clientIDList.remove(v.getId());
+		//					orders.remove(v.getId());
+		//					sendAlert(clientIDList.get(v.getId()));
+		//				}
+		//			});
+		//			
+		//			this.setContentView(scrollview);
+		//
+		//		}
 	}
-	
+
 	private void sendAlert(int clientID){
 		ConnectionApplication app = (ConnectionApplication) getApplication();
 		//byte buf[] = new byte[msg.length() + 2];
 		byte buf[] = new byte[2];
 		buf[0] = (byte)clientID; 
 		buf[1] = (byte)5; // 5 means order is completed; 
-		
+
 
 		// Now send through the output stream of the socket
 		OutputStream out;
